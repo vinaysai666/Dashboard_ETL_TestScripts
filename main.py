9f4a1e5eada9d42d52a9da8e110ad794448e26d6
vinaysai666 <vinays@newstreettech.com>
2025-01-30 11:43:58 +0530

    Report Generator Via Outlook

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from validation.validator import validate_data
+from  utils.email_generator import email_generation
 
 # Run data validation
 if __name__ == "__main__":
     validate_data("testcases.json")
+    email_generation()
+    
